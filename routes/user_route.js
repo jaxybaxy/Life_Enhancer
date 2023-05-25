b0e31c2bc171cbb578a8058e0606d20a10f8981c
jaxybaxy <momen.ahmed_2010@yahoo.com>
2023-05-25 21:18:43 +0300

    google auth

--- routes/user_route.js
+++ routes/user_route.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const userController = require("../controllers/user_controller")
 const verify =require("../services/request-handler")
 router.post("/signup", userController.signUp)
-// router.post("/gmailAuth", userController.signInWithGoogle)
+router.post("/gmailAuth", userController.signInWithGoogle)
 router.post("/login", userController.logIn)
 router.post("/update", userController.updateUser)
 router.get("/allUsers", userController.getAllUsers)
 router.get("/:userid",verify.verifyToken, userController.getUserById)
 router.delete("/delete",userController.deleteUserById)
+router.get("/:userid",verify.verifyToken, userController.getUserById)
+
 module.exports = router;
