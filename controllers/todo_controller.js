e92ef9c43ed33230ab0cddb64ffcb24046cfdbc5
jaxybaxy <momen.ahmed_2010@yahoo.com>
2023-05-29 21:59:27 +0300

    todo_update

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -5,10 +5,11 @@ exports.dailyTask = async (req, res) => {
     try {
         const email= req.user.email
         const user = await UserModel.findOne({email})
-        
+        // const date = new Date.now()
+        // console.log(date.getDay())
         var level = user.exerciceLevel
         const exercises = await ExerciseModel.find({ Level: level }).limit(10);
-        res.status(200).send({ status: true, exercise: exercises, diet:user.diet})}
+        res.status(200).send({ status: true, exercise: exercises})}
 
     catch (err) {
             return res.send({ status: false, error: 'failed to list exercise' })
