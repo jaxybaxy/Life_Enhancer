9fd61f08e433f1f56932f7f722f4f17019d42dcd
jaxybaxy <momen.ahmed_2010@yahoo.com>
2023-07-01 17:21:22 +0300

    guide update

--- controllers/guide_controller.js
+++ controllers/guide_controller.js
@@ -1,25 +1,27 @@
 const guideModel = require("../models/guide_model")
 
-exports.getGuide = async (req,res) => {
-    try{
-        var hasDisorder = req.body.hasDisorder
+exports.getGuide = async (req, res) => {
+    try {
+        // var hasDisorder = req.body.hasDisorder
         const guide = await guideModel.find()
 
-        if (!hasDisorder){
-            return res.status(500).send({ error: "hasDisorder is empty" })
-        }
-        if(hasDisorder==1){
-            return res.status(200).send({status:true,guide:guide})
-        }
-        else if (hasDisorder==0){
-            return res.status(200).send({status:true})
+        // if (!hasDisorder){
+        //     return res.status(500).send({ error: "hasDisorder is empty" })
+        // }
+        // if(hasDisorder==1){
+        //     return res.status(200).send({status:true,guide:guide})
+        // }
+        // else if (hasDisorder==0){
+        //     return res.status(200).send({status:true})
 
-        }
-        else{
-        return res.status(500).send({status:false, error: 'invalid input' })
-        }
+        // }
+        // else{
+        // return res.status(500).send({status:false, error: 'invalid input' })
+        // }
+        return res.status(200).send({ status: true, guide: guide })
+
     }
     catch (error) {
-        return res.send({ status:false,error: 'failed to list guide' })
+        return res.send({ status: false, error: 'failed to list guide' })
     }
-  }
+}
